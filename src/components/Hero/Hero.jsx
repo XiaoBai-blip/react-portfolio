b9b869c2575e26a5e38d6934d755a928c5d2b16e
Xiao Bai <rachelbai413@gmail.com>
2025-01-06 17:05:56 -0800

    11
    11

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,10 @@ export const Hero = () => {
   return (
     <section className={styles.contanier}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, i'm Xiao (Rachel)</h1>
-        <p className={styles.description}>xxxxxxx</p>
+        <h1 className={styles.title}>Hi, i'm Xiao Bai (Rachel)</h1>
+        <p className={styles.description}>A passionate data scientist and software developer with a knack for learning new stuff. My journey has been shaped by diverse experiences, including three data analyst internships where I I’ve driven impactful result with tools like Python, SQL, and Tableau. </p>
+        <p className={styles.description}>I’ve tackled exciting challenges like building a Plant Leaf Disease AI model with cutting-edge computer vision and creating a real-time chat app using the MERN stack. These projects sharpened my skills in Python, PyTorch, and full-stack development, fueling my passion for innovation and impactful solutions.</p>
+        <p className={styles.description}>I’m currently exploring new opportunities in tech industry. I’m eager to contribute to meaningful projects and continue pushing the boundaries of what’s possible with data and technology!</p> 
         <a href="mailto:xiaobai@usc.edu" className={styles.contactBtn}>
           Contact Me
         </a>
